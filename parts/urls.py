ec306d8f0a8aa8f6e75bf58a95e205304ffd809c
Mqxl <53327474+Mqxl@users.noreply.github.com>
2021-12-20 12:43:11 +0600

    Add files via upload

--- parts/urls.py
+++ parts/urls.py
@@ -1,25 +1,24 @@
-from django.urls import path
-from django.conf.urls import url
+
 from django.urls.conf import re_path
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.product_list, name='view'),
-    url('signup', views.signup, name='signup'),
-    url('contact', views.contact, name='contact'),
-    url('about', views.about, name='about'),
-    url(r'^(?P<category_slug>[-\w]+)/$',
+    re_path(r'^$', views.product_list, name='view'),
+    re_path('signup', views.signup, name='signup'),
+    re_path('contact', views.contact, name='contact'),
+    re_path('about', views.about, name='about'),
+    re_path(r'^(?P<category_slug>[-\w]+)/$',
         views.product_list,
         name='product_list_by_category'),
-    url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$',
+    re_path(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$',
         views.product_detail,
         name='product_detail'),
-    url('person', views.personal, name='personal'),
-    url('updateimage', views.updateimage, name='updateimage'),
-    url(r'order/(?P<id>\d+)',views.detail_view,name='detail_view'),
+    re_path('person', views.personal, name='personal'),
+    re_path('updateimage', views.updateimage, name='updateimage'),
+    re_path(r'order/(?P<id>\d+)',views.detail_view,name='detail_view'),
    
     re_path(r'check/(?P<id>\d+)', views.Pdf.as_view()),
-    url('logou',views.logout,name='logout'),
+    re_path('logou',views.logout,name='logout'),
     
 
 
